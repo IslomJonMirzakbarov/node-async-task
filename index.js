7b4d12e1ec0b63ee4ef3f546f1ebf436bca9a353
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-08-21 17:53:01 +0900

    second part finished

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const crypto = require("crypto");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there!");
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("Hi there!");
+  });
+});
+
+app.get("/fast", (req, res) => {
+  res.send("This was fast!");
 });
 
 app.listen(3000);
